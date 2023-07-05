fe580d5de5d39300cbef8d10797b39b30d27b827
Tarak Ram <jujjurutarakram1818@gmail.com>
2023-07-05 11:29:02 +0530

    init

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import json
+import os
 
 # Load the index
 # index_path = '/Users/tarakram/Documents/Chatbot/index'
@@ -7,8 +8,7 @@ import json
 #     index = json.load(f)
 from llama_index import StorageContext, load_index_from_storage
 
-import os
-os.environ["OPENAI_API_KEY"] = 'your api key '
+os.environ["OPENAI_API_KEY"] = 'Add your API Key here '
 
 # rebuild storage context
 storage_context = StorageContext.from_defaults(persist_dir='/Users/tarakram/Documents/Chatbot/index')
@@ -22,10 +22,12 @@ import openai
 import json
 
 class Chatbot:
-    def __init__(self, api_key, index):
+    def __init__(self, api_key, index, user_id):
         self.index = index
         openai.api_key = api_key
+        self.user_id = user_id
         self.chat_history = []
+        self.filename = f"{self.user_id}_chat_history.json"
 
     def generate_response(self, user_input):
         prompt = "\n".join([f"{message['role']}: {message['content']}" for message in self.chat_history[-5:]])
@@ -38,50 +40,51 @@ class Chatbot:
         self.chat_history.append(message)
         return message
     
-    def load_chat_history(self, filename):
+    def load_chat_history(self):
         try:
-            with open(filename, 'r') as f:
+            with open(self.filename, 'r') as f:
                 self.chat_history = json.load(f)
         except FileNotFoundError:
             pass
 
-    def save_chat_history(self, filename):
-        with open(filename, 'w') as f:
+    def save_chat_history(self):
+        with open(self.filename, 'w') as f:
             json.dump(self.chat_history, f)
             
-bot = Chatbot("your api key ", index)
-
 # Streamlit app
 def main():
-    st.title(" MysteryVibe Chatbot")
+    st.title("Chatbot")
 
-    # Load chat history
-    bot.load_chat_history("/Users/tarakram/Documents/Chatbot/chat_history.json")
+    # User ID
+    user_id = st.text_input("Your Name:")
+    
+    # Check if user ID is provided
+    if user_id:
+        # Create chatbot instance for the user
+        bot = Chatbot("Add your API Key here ", index, user_id)
 
-    # Display chat history
-    chat_history = st.empty()
-    for message in reversed(bot.chat_history):
-        chat_history.write(f"{message['role']}: {message['content']}")
+        # Load chat history
+        bot.load_chat_history()
 
-    # User input
-    user_input = st.text_input("You:")
+        # Display chat history
+        for message in bot.chat_history[-6:]:
+            st.write(f"{message['role']}: {message['content']}")
 
-    # Send button
-    col1, col2, col3 = st.columns([1, 6, 1])
-    if col2 or col1 or col3 .button("Send"):
-        if user_input.lower() in ["bye", "goodbye"]:
-            bot_response = "Goodbye!"
-        else:
-            bot_response = bot.generate_response(user_input)
-            bot_response_content = bot_response['content']
-            bot.chat_history.append({"role": "user", "content": user_input})
-            bot.chat_history.append({"role": "assistant", "content": bot_response_content})
-            chat_history.text(f"You: {user_input}")
-            chat_history.text(f"Bot: {bot_response_content}")
-            bot.save_chat_history("/Users/tarakram/Documents/Chatbot/chat_history.json")
+        # User input
+        user_input = st.text_input("Type your questions here :) - ")
 
-    # Save chat history
-    bot.save_chat_history("/Users/tarakram/Documents/Chatbot/chat_history.json")
+        # Generate response
+        if user_input:
+            if user_input.lower() in ["bye", "goodbye"]:
+                bot_response = "Goodbye!"
+            else:
+                bot_response = bot.generate_response(user_input)
+                bot_response_content = bot_response['content']
+                st.write(f"{user_id}: {user_input}")
+                st.write(f"Bot: {bot_response_content}")
+                bot.save_chat_history()
+                bot.chat_history.append({"role": "user", "content": user_input})
+                bot.chat_history.append({"role": "assistant", "content": bot_response_content})
 
 if __name__ == "__main__":
     main()
